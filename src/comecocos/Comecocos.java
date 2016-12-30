994cdb06c57451648f6de0d16d1882434a8563ed
llanitos <llanosnunez@gmail.com>
2016-12-30 10:09:57 +0100

    First commit

---
+++ src/comecocos/Comecocos.java
@@ -0,0 +1,24 @@
+package comecocos;
+import static comecocos.ES.*;
+
+/**
+ *
+ * @author llanos
+ */
+
+public class Comecocos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String mensaje = "Escriba un numero entero: ";
+        int leeEntero = leeEntero(mensaje);
+        int numero = leeEntero;
+        
+        System.out.print(numero);
+        
+    }
+    
+}
